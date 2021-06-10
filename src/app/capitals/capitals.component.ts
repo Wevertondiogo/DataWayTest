32c8dcdc6ea5c7873755a4282c0be62202072b2c
Weverton Diogo Procópio da Cruz <weverton@pop-os.localdomain>
2021-06-10 15:32:48 -0300

    function exportDataGrid was created

--- src/app/capitals/capitals.component.ts
+++ src/app/capitals/capitals.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Cities } from '../models/cities.models';
 import { CapitalsService } from '../services/capitals.service';
 
+import { Workbook } from 'exceljs';
+import saveAs from 'file-saver';
+import { exportDataGrid } from 'devextreme/excel_exporter';
+
 @Component({
   selector: 'app-capitals',
   templateUrl: './capitals.component.html',
@@ -16,6 +20,21 @@ export class CapitalsComponent implements OnInit {
       this.districts = results;
       console.log(results)
     })
-  }
+  
 
+}
+
+exportGrid(e) {
+  const workbook = new Workbook(); 
+  const worksheet = workbook.addWorksheet("Main sheet"); 
+  exportDataGrid({ 
+      worksheet: worksheet, 
+      component: e.component
+  }).then(function() {
+      workbook.xlsx.writeBuffer().then(function(buffer: BlobPart) { 
+          saveAs(new Blob([buffer], { type: "application/octet-stream" }), "DataGrid.xlsx"); 
+      }); 
+  }); 
+  e.cancel = true; 
+}
 }
